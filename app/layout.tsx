1b248483fd424b20d5bf801195b7c73151799c29
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-05-17 08:50:56 +0900

    rename: 헤더 폴더명 변경 및 경로 변경
    - 헤더 폴더명 변경 및 경로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/layout/Header/Header";
+import Header from "./components/layout/header/Header";
 import { Urbanist } from "next/font/google";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
