ed606ebce3d28d62f991c5a8ea1ac86f8e2758c1
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-05-17 22:00:16 +0900

    feat: metamask 지갑 연동
    - connet wallet 버튼 클릭 시, metamask 지갑 연동
    - metamask 미설치 시, metamask 페이지 띄우기
    
    close #3

--- app/components/layout/Header/Header.tsx
+++ app/components/layout/Header/Header.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "../../common/Logo";
 import Modal from "../../common/Modal/Modal";
 import Profile from "../../profile/Profile";
 import styles from "./Header.module.scss";
 import Button from "../../common/Button/Button";
+import { useWeb3React } from "@web3-react/core";
 
 const Header = () => {
   const [isVisibleModal, setIsVisibleModal] = useState(false);
-
   const connectWallet = () => {};
+  const context = useWeb3React();
 
   const ButtonProps = {
     width: 200,
@@ -20,6 +21,15 @@ const Header = () => {
     text: "Connect Wallet",
     handleClick: connectWallet,
   };
+  context.connector.deactivate;
+  console.log({ ...context });
+
+  useEffect(() => {
+    if ((window as any).ethereum === undefined) {
+      // 지갑이 설치 안되어있으면 설치 페이지를 오픈
+      window.open("https://metamask.io/download.html");
+    }
+  });
 
   return (
     <div className={styles.container}>
@@ -27,15 +37,22 @@ const Header = () => {
         <Logo width={36} height={20} />
         <span className={styles.title}>Renaissance Lab.</span>
       </div>
-      <div className={styles.profile}>
-        <Profile setIsVisibleModal={setIsVisibleModal} />
-        {isVisibleModal && (
-          <Modal>
-            <span className={styles.modal__title}>Connect</span>
-            <Button {...ButtonProps}></Button>
-          </Modal>
-        )}
-      </div>
+      {
+        // 지갑 활성화 유무에 따른 프로필 변화
+        !context.isActive ? (
+          <div className={styles.profile}>
+            <Profile setIsVisibleModal={setIsVisibleModal} />
+            {isVisibleModal && (
+              <Modal>
+                <span className={styles.modal__title}>Connect</span>
+                <Button {...ButtonProps}></Button>
+              </Modal>
+            )}
+          </div>
+        ) : (
+          <div></div>
+        )
+      }
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Header from "./components/layout/Header/Header";
 import { Urbanist } from "next/font/google";
+import { Web3ReactHooks, Web3ReactProvider } from "@web3-react/core";
+import type { MetaMask } from "@web3-react/metamask";
+import { hooks as metaMaskHooks, metaMask } from "./components/connectors/metaMask";
 
 const urbanist = Urbanist({ weight: ["200", "400", "700"], subsets: ["latin"] });
 
@@ -7,13 +12,16 @@ export const metadata = {
   title: "NFT",
   description: "NFT exchange",
 };
+const connectors: [MetaMask, Web3ReactHooks][] = [[metaMask, metaMaskHooks]];
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={urbanist.className}>
-        <Header />
-        {children}
+        <Web3ReactProvider connectors={connectors}>
+          <Header />
+          {children}
+        </Web3ReactProvider>
       </body>
     </html>
   );
